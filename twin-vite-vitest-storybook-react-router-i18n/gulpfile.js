1b107f2706b67039de53382a35caa92cd56d5dcf
Hansanghyeon <999@hyeon.pro>
2023-11-14 00:23:53 +0000

    feat(i18n): i18n lexer 멀티플하게 할수있도록 변경

--- twin-vite-vitest-storybook-react-router-i18n/gulpfile.js
+++ twin-vite-vitest-storybook-react-router-i18n/gulpfile.js
@@ -2,65 +2,76 @@ import gulp from 'gulp'
 import { gulp as i18nextParser } from 'i18next-parser'
 import fs from 'fs'
 import path from 'path'
-import i18nextConfig from './.i18n/i18next-parser.config.js'
+import namespaceI18nextConfig from './.i18n/namespace/i18next-parser.config.js'
 
-gulp.task('i18next', (done) => {
-  gulp
-    .src('src/**')
-    .pipe(
-      // eslint-disable-next-line new-cap
-      new i18nextParser(i18nextConfig)
-    )
-    .pipe(gulp.dest('./'))
-  done()
-})
-
-gulp.task('i18next-cleanup', (done) => {
-  try {
-    fs.writeFileSync('locales/translation/dev.json', '{}', 'utf8')
-    console.log('Clean locales/translation/dev.json file.')
+// ================================================ADMIN START
+const genI18nextConfigToTaskRun = (namespace, i18nextConfig) => {
+  gulp.task(`${namespace}:i18next`, (done) => {
+    gulp
+      .src('src/**')
+      .pipe(
+        new i18nextParser(i18nextConfig)
+      )
+      .pipe(gulp.dest('./'))
     done()
-  } catch (error) {
-    console.error(
-      'Error parsing or updating the `locales/translation/dev.json` file:',
-      error
-    )
-    throw error
-  }
-})
-
-gulp.task('update-json', (done) => {
-  const file = path.join('./locales/translation/dev.json')
-  // Read the JSON file
-  const data = fs.readFileSync(file, 'utf8')
-
-  try {
-    // Parse the JSON data
-    const jsonData = JSON.parse(data)
-
-    // 객체의 키값을 value에 동일하게 넣기
-    Object.keys(jsonData).forEach((key) => {
-      jsonData[key] = key
-    })
-
-    // 덮어씌워야한다
-    fs.writeFileSync(
-      file,
-      JSON.stringify(jsonData),
-      { encoding: 'utf8', flag: 'w' },
-      (err) => {
-        if (err) {
-          throw err
+  })
+  
+  gulp.task(`${namespace}:i18next-cleanup`, (done) => {
+    try {
+      fs.writeFileSync(`locales/${namespace}/dev.json`, '{}', 'utf8')
+      console.log(`Clean locales/${namespace}/dev.json file.`)
+      done()
+    } catch (error) {
+      console.error(
+        `Error parsing or updating the 'locales/${namespace}/dev.json' file:`,
+        error
+      )
+      throw error
+    }
+  })
+  
+  gulp.task(`${namespace}:update-json`, (done) => {
+    const file = path.join(`./locales/${namespace}/dev.json`)
+    // Read the JSON file
+    const data = fs.readFileSync(file, 'utf8')
+  
+    try {
+      // Parse the JSON data
+      const jsonData = JSON.parse(data)
+  
+      // 객체의 키값을 value에 동일하게 넣기
+      Object.keys(jsonData).forEach((key) => {
+        jsonData[key] = key
+      })
+  
+      // 덮어씌워야한다
+      fs.writeFileSync(
+        file,
+        JSON.stringify(jsonData),
+        { encoding: 'utf8', flag: 'w' },
+        (err) => {
+          if (err) {
+            throw err
+          }
         }
-      }
-    )
+      )
+  
+      console.log('File successfully updated.')
+      done() // Signal task completion
+    } catch (error) {
+      console.error('Error parsing or updating the JSON file:', error)
+      done(error) // Signal error and task completion
+    }
+  })
 
-    console.log('File successfully updated.')
-    done() // Signal task completion
-  } catch (error) {
-    console.error('Error parsing or updating the JSON file:', error)
-    done(error) // Signal error and task completion
-  }
-})
+  return [`${namespace}:i18next-cleanup`, `${namespace}:i18next`, `${namespace}:update-json`]
+}
 
-gulp.task('default', gulp.series('i18next-cleanup', 'i18next', 'update-json'))
+
+// namepsace lexer 만들어서 가져오기
+const translationSeries = genI18nextConfigToTaskRun('translation', namespaceI18nextConfig)
+// const glossarySeries = genI18nextConfigToTaskRun('glossary', glossaryI18nextConfig)
+gulp.task('default', gulp.series([
+  ...translationSeries,
+  // ...glossarySeries
+]))
