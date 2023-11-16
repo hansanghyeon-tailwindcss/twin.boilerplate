9ebb3454c131ceb6f2c4323b4252bdf1380a644c
Hansanghyeon <999@hyeon.pro>
2023-11-16 08:58:57 +0000

    fix: Failed to fetch dynamically imported module
    이미 import.meta.glob로 가져왔을때 https://vitejs.dev/guide/features.html\#glob-import으로 잘가져왔는데 키값으로 다시 import를하려면서 번들링할때 해당 파일을 찾지못하는 이슈인것같다. 가져온 그대로 사용하는 방식으로 변경

--- twin-vite-vitest-storybook-react-router-i18n/src/routing/router.tsx
+++ twin-vite-vitest-storybook-react-router-i18n/src/routing/router.tsx
@@ -1,40 +1,42 @@
 import React, { Suspense } from 'react'
 import { pipe } from 'fp-ts/function'
 import * as A from 'fp-ts/Array'
+import * as R from 'fp-ts/Record'
 import { type RouteWithChildrenInterface } from './routing-interface'
+import { ErrorBoundary } from 'react-error-boundary'
 
 // ============================================= ROUTER START
 // 파일기반의 라우팅을 위한 glob 패턴
 const pages = pipe(
   import.meta.glob('/src/pages/**/[a-z[]*.tsx'),
+  R.filterWithIndex((k) => !k.includes('auth') && !k.includes('api')),
   // 파일 목록만 가져오는 방법
-  Object.keys,
-  A.map(e => {
-    return {
-      path: e.replace(/\/src\/pages|index|\.tsx$/g, '').replace(/\[\.{3}.+\]/, '*').replace(/\[(.+)\]/, ':$1'),
-      filePath: e
-    }
-  })
-);
+  Object.entries,
+  A.map(([key, module]) => ({
+    path: key
+      .replace(/\/src\/pages|index|\.tsx$/g, '')
+      .replace(/\[\.{3}.+\]/, '*')
+      .replace(/\[(.+)\]/, ':$1'),
+    module,
+  }))
+)
 
-const DynamicComponent = ({ path }: { path: string }) => {
-  const LazyComponent = React.lazy(() => import(/* @vite-ignore */path));
+const DynamicComponent = ({ module }: { module: any }) => {
+  const LazyComponent = React.lazy(module)
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <LazyComponent />
     </Suspense>
-  );
+  )
 }
 
 const router: RouteWithChildrenInterface[] = pipe(
   pages,
-  A.map(({ path, filePath }) => {
-    return {
-      path: (locale: string) => `/${locale}${path}`,
-      element: <DynamicComponent path={filePath} />,
-    }
-  })
+  A.map(({ path, module }) => ({
+    path: (locale: string) => `/${locale}${path}`,
+    element: <DynamicComponent module={module} />,
+  }))
 )
 
 export default router
